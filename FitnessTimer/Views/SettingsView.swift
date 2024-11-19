8405ea024334b9f958084c365e3190c623e53d53
james-du <muzikjay@gmail.com>
2024-11-18 20:38:23 -0800

    Present number picker when tapping timer arrow in Settings

--- FitnessTimer/Views/SettingsView.swift
+++ FitnessTimer/Views/SettingsView.swift
@@ -46,8 +46,10 @@ struct SettingsView: View  {
             // MARK: Timer Settings
             VStack(alignment: .leading, spacing: 20) {
                 // TODO: FIX ALIGNMENT SO TIME ROWS ARE LEFT-ALIGNED
+                // The negative leading padding is a temporary fix
                 RoundTimeView()
                 RestTimeView()
+                    .padding(.leading, -10)
             }
             .safeAreaPadding()
             Spacer()
@@ -60,9 +62,12 @@ struct SettingsView: View  {
 }
 
 
-// TODO: SEE ABOUT MAKING THIS A REUSABLE VIEW SINCE THE LAYOUT IS IDENTICAL MINUS THE DATA AND TRACKED TIMER
+// TODO: SEE ABOUT MAKING THIS A REUSABLE VIEW SINCE THE LAYOUT FOR ROUND AND REST TIME IS IDENTICAL EXCEPT FOR THE DATA AND TIMER BEING TRACKED
 struct RoundTimeView: View, TimeDisplaying {
     @EnvironmentObject var timerStateManager: TimerStateManager
+    @State var editModeEnabled: Bool = false
+    @State var minutesSelected: Int = 0
+    @State var secondsSelected: Int = 0
     var timerType: TimerType = .roundTimer
 
     var body: some View {
@@ -78,9 +83,36 @@ struct RoundTimeView: View, TimeDisplaying {
 
             Spacer()
             Spacer()
-            Button(action: { print("Button tapped") }) {
+            Button(action: { editModeEnabled.toggle() }) {
                 Image(systemName: "chevron.forward")
                     .imageScale(.large)
+            }
+            .sheet(isPresented: $editModeEnabled) {
+                HStack {
+                    HStack {
+                        Text("Minutes: ")
+                        Picker("Minutes", selection: $minutesSelected) {
+                            ForEach(0..<60, id: \.self) { number in
+                                Text("\(number)").tag(number)
+                                    .foregroundStyle(.black)
+                            }
+                            .pickerStyle(.wheel)
+                            .frame(maxWidth: .infinity, maxHeight: 300)
+                        }
+                    }
+
+                    HStack {
+                        Text("Seconds: ")
+                        Picker("Seconds", selection: $secondsSelected) {
+                            ForEach(0..<60, id: \.self) { number in
+                                Text("\(number)").tag(number)
+                            }
+                            .pickerStyle(.wheel)
+                            .frame(maxWidth: .infinity, maxHeight: 300)
+                        }
+                    }
+                }
+                .background(.clear)
             }
         }
     }
