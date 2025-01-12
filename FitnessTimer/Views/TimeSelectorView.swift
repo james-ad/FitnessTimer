34e1bd11269979a26acdc9fba77589ae0da06172
james-du <muzikjay@gmail.com>
2025-01-11 23:39:09 -0800

    Add exit buttons to time selection views

--- FitnessTimer/Views/TimeSelectorView.swift
+++ FitnessTimer/Views/TimeSelectorView.swift
@@ -8,34 +8,63 @@
 import SwiftUI
 
 struct TimeSelectorView: View {
+    @Environment(\.dismiss) var dismiss
     @State var minutesSelected: Int = 0
     @State var secondsSelected: Int = 0
 
     var body: some View {
-        HStack {
+        VStack {
+            Spacer()
+
             HStack {
-                Text("Minutes: ")
-                Picker("Minutes", selection: $minutesSelected) {
-                    ForEach(0..<60, id: \.self) { number in
-                        Text("\(number)").tag(number)
-                            .foregroundStyle(.black)
+                HStack {
+                    Text("Minutes: ")
+                        .foregroundStyle(.white)
+                    Picker("Minutes", selection: $minutesSelected) {
+                        ForEach(0..<60, id: \.self) { number in
+                            Text("\(number)").tag(number)
+                                .foregroundStyle(.white)
+                        }
+                        .pickerStyle(.wheel)
+                        .background(.black)
+                        .frame(maxWidth: .infinity, maxHeight: 300)
                     }
-                    .pickerStyle(.wheel)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
+                }
+                
+                HStack {
+                    Text("Seconds: ")
+                        .foregroundStyle(.white)
+                    Picker("Seconds", selection: $secondsSelected) {
+                        ForEach(0..<60, id: \.self) { number in
+                            Text("\(number)").tag(number)
+                        }
+                        .pickerStyle(.wheel)
+                        .frame(maxWidth: .infinity, maxHeight: 300)
+                    }
                 }
             }
 
-            HStack {
-                Text("Seconds: ")
-                Picker("Seconds", selection: $secondsSelected) {
-                    ForEach(0..<60, id: \.self) { number in
-                        Text("\(number)").tag(number)
-                    }
-                    .pickerStyle(.wheel)
-                    .frame(maxWidth: .infinity, maxHeight: 300)
-                }
+            Spacer()
+
+            Button(action: {
+                print("Button tapped")
+                dismiss()
+            }) {
+                Text("Exit Settings")
+                    .font(.callout)
+                    .padding(12)
+                    .foregroundStyle(.black)
+                    .background(.white)
+                    .clipShape(RoundedRectangle(cornerRadius: 8))
             }
+            .buttonBorderShape(.roundedRectangle)
         }
-        .background(.clear)
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(.black)
+        .safeAreaPadding()
     }
+}
+
+#Preview {
+    TimeSelectorView()
 }
