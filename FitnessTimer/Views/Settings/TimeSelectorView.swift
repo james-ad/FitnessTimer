a2ebb791eea36af92762102b86fdb6aad896aec0
james-du <muzikjay@gmail.com>
2025-02-26 22:04:51 -0800

    Improve UI for TimerSettingsView and organize code a bit

--- FitnessTimer/Views/Settings/TimeSelectorView.swift
+++ FitnessTimer/Views/Settings/TimeSelectorView.swift
@@ -23,16 +23,20 @@ struct TimeSelectorView: View {
         self.timerType = timerType
         self._storedTime = storedTime
     }
+
     
-    
-    // MARK: Temp constants until I can organize them better
+    // TODO: Temp constants until I can organize them better
     private let pickerHorizontalPadding: CGFloat = 100
-    private let kerning: CGFloat = 3
     private let pickerHorizontalOffset: CGFloat = 10
-    
+    private var kerning: CGFloat {
+        UIScreen.main.bounds.width <= 375 ? 1.5 : 3
+    }
+    private var vStackSpacing: CGFloat {
+        UIScreen.main.bounds.width <= 375 ? -4 : 1
+    }
+
     var body: some View {
-        ZStack(alignment: .center) {
-            VStack(alignment: .center, spacing: 1) {
+            VStack(alignment: .center, spacing: vStackSpacing) {
                 Spacer()
                 Text(title)
                     .foregroundStyle(.white)
@@ -40,55 +44,75 @@ struct TimeSelectorView: View {
                     .kerning(5)
 
                 // MARK: Minutes
-                HStack {
-                        Picker("Minutes", selection: $storedTime.minutes) {
-                            ForEach(0..<60, id: \.self) { number in
-                                Text(String(format: "%02d", number)).tag(number)
-                                    .kerning(kerning)
-                                    .foregroundStyle(.white)
-                                    .font(.title)
-                                    .offset(x: pickerHorizontalOffset)
-                            }
-                            .background(.black)
+                HStack(alignment: .center) {
+                    Picker("Minutes", selection: $storedTime.minutes) {
+                        ForEach(0..<60, id: \.self) { number in
+                            Text(String(format: "%02d", number)).tag(number)
+                                .kerning(kerning)
+                                .foregroundStyle(.white)
+                                .font(.title)
+                                .fixedSize()
+                                .offset(x: pickerHorizontalOffset)
                         }
-                        .pickerStyle(.wheel)
-                        .padding(.leading, pickerHorizontalPadding)
-                        
-                        Text(":")
-                            .foregroundStyle(.white)
-                            .font(.largeTitle)
-                            .background(.clear)
+                        .background(.black)
+                    }
+                    .pickerStyle(.wheel)
+                    .padding(.leading, pickerHorizontalPadding)
+
+                    Text(":")
+                        .foregroundStyle(.white)
+                        .font(.largeTitle)
+                        .offset(y: -3.175)
+                        .background(.clear)
 
                     // MARK: Seconds
-                        Picker("Seconds", selection: $storedTime.seconds) {
-                            ForEach(0..<60, id: \.self) { number in
-                                Text(String(format: "%02d", number)).tag(number)
-                                    .kerning(kerning)
-                                    .foregroundStyle(.white)
-                                    .font(.title)
-                                    .offset(x: -pickerHorizontalOffset)
-                            }
-                            .background(.black)
+                    Picker("Seconds", selection: $storedTime.seconds) {
+                        ForEach(0..<60, id: \.self) { number in
+                            Text(String(format: "%02d", number)).tag(number)
+                                .kerning(kerning)
+                                .foregroundStyle(.white)
+                                .font(.title)
+                                .fixedSize()
+                                .offset(x: -pickerHorizontalOffset)
                         }
-                        .pickerStyle(.wheel)
-                        .padding(.trailing, pickerHorizontalPadding)
+                        .background(.black)
+                    }
+                    .pickerStyle(.wheel)
+                    .padding(.trailing, pickerHorizontalPadding)
+                }
+
+                HStack(alignment: .center) {
+                    Text("Minutes")
+                        .foregroundStyle(.white)
+                        .font(.caption)
+                        .padding(.trailing, 3)
+
+                    Text("Seconds")
+                        .foregroundStyle(.white)
+                        .font(.caption)
+                        .padding(.leading, 3)
                 }
 
                 Spacer()
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity)
-        }
-        .frame(maxWidth: .infinity, maxHeight: .infinity)
         .background(.black)
         .safeAreaPadding()
     }
 }
 
 #Preview {
-    @Previewable @State var timerStateManager = TimerStateManager()
-    TimeSelectorView(
-        timerType: .roundTimer,
-        storedTime: $timerStateManager.roundPickerTime
+    @Previewable @Bindable var timerStateManager = TimerStateManager()
+    TimerSettingsView(timerStateManager: $timerStateManager,
+                      setTime: timerStateManager.setTime
     )
-    .environment(timerStateManager)
 }
+
+//#Preview {
+//    @Previewable @State var timerStateManager = TimerStateManager()
+//    TimeSelectorView(
+//        timerType: .roundTimer,
+//        storedTime: $timerStateManager.roundPickerTime
+//    )
+//    .environment(timerStateManager)
+//}
